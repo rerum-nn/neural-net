2e65e84cbe2a8c188fb7932dd92c5401cf5828fc
Васенев Владимир Олегович <vovasenev@edu.hse.ru>
2024-04-20 21:07:49 +0300

    optimizers

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,7 @@
 #include "../src/Layers/Softmax.h"
 #include "../src/LossFunctions/BinaryCrossEntropy.h"
 #include "../src/Network.h"
+#include "../src/Optimizers/Optimizers.h"
 #include "../src/Types.h"
 
 #include <gtest/gtest.h>
@@ -13,16 +14,12 @@ using namespace neural_net;
 
 TEST(Models, XOR) {
     Network network({Linear(2, 2), Sigmoid(), Linear(2, 1), Sigmoid()});
-    Matrix train_data{
-        {0, 1, 0, 1},
-        {0, 0, 1, 1}
-    };
-    Matrix expected_answers{
-        {0, 1, 1, 0}
-    };
-    for (size_t i = 1; i < 100000;++ i) {
-        network.Fit(train_data, expected_answers, BinaryCrossEntropy());
-    }
+    Matrix train_data{{0, 1, 0, 1}, {0, 0, 1, 1}};
+    Matrix labels{{0, 1, 1, 0}};
+
+    Network network2 = network;
+    Optimizer adam = Optimizers::Adam(0.03);
+    adam(network, train_data, labels, BinaryCrossEntropy(), 1000);
     for (Index i = 0; i < 4; ++i) {
         Vector vector = train_data.col(i);
         Vector res;
@@ -32,13 +29,12 @@ TEST(Models, XOR) {
             std::cout << j << ": " << res[j] << std::endl;
         }
     }
+
 }
 
 TEST(CheckLayers, Softmax) {
     Network network({Softmax()});
-    Vector data{
-        {1000, 2000, 3000}
-    };
+    Vector data{{1000, 2000, 3000}};
     Vector ans = network.Predict(data);
     for (Index i = 0; i < ans.rows(); ++i) {
         std::cout << i << ": " << ans[i] << std::endl;
