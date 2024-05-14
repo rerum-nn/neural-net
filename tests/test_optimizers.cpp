c9173fc2e3799f502271dffb3d9d0f036ed5b8b8
Васенев Владимир Олегович <vovasenev@edu.hse.ru>
2024-05-14 11:33:20 +0300

    optimizers tests

--- tests/test_optimizers.cpp
+++ tests/test_optimizers.cpp
@@ -1,40 +1,32 @@
-#include "Layers/Activations/Sigmoid.h"
 #include "Optimizers/Optimizer.h"
 #include "TestHelpers.h"
 #include "Types.h"
 
-#include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
 
 using neural_net::Index;
 using neural_net::Matrix;
+using neural_net::Vector;
 using neural_net::Optimizer;
-using neural_net::ParametersGrad;
-using neural_net::Sigmoid;
-using testing::DoubleNear;
+using neural_net::UpdatePack;
+using neural_net::Linear;
 
 TEST(SGD, SingleStep) {
     Optimizer sgd = Optimizer::SGD(0.5);
     Matrix vars{{1.0, 2.0, 3.0, 4.0}};
-    ParametersGrad grads{vars, Matrix{{1.0, 6.0, 7.0, 2.0}}};
-    sgd->InitParameters({Sigmoid()});
+    Vector bias{{0.0}};
+    UpdatePack grads{vars, Matrix{{1.0, 6.0, 7.0, 2.0}}, bias, Vector{{0}}};
+    sgd->InitParameters({Linear(4, 1)});
     sgd->Update({grads}, 0);
     CheckCloseMatrix(vars, Matrix{{0.5, -1.0, -0.5, 3.0}});
-}
-
-TEST(SGD, EmptyGrad) {
-    Optimizer sgd = Optimizer::SGD(0.5);
-    Matrix vars{{1.0, 2.0, 3.0, 4.0}};
-    ParametersGrad grads{vars, Matrix{}};
-    sgd->InitParameters({Sigmoid()});
-    ASSERT_DEATH(sgd->Update({grads}, 0), "");
 }
 
 TEST(Adam, SingleStep) {
     Optimizer adam = Optimizer::Adam(0.5);
     Matrix vars{{1.0, 2.0, 3.0, 4.0}};
-    ParametersGrad grads{vars, Matrix{{1.0, 6.0, 7.0, 2.0}}};
-    adam->InitParameters({Sigmoid()});
+    Vector bias{{0}};
+    UpdatePack grads{vars, Matrix{{1.0, 6.0, 7.0, 2.0}}, bias, Vector{{0}}};
+    adam->InitParameters({Linear(4, 1)});
     adam->Update({grads}, 0);
     CheckCloseMatrix(vars, Matrix{{0.5, 1.5, 2.5, 3.5}});
 }
@@ -42,8 +34,9 @@ TEST(Adam, SingleStep) {
 TEST(RMSProp, SingleStep) {
     Optimizer rmsprop = Optimizer::RMSprop(0.5);
     Matrix vars{{1.0, 2.0, 3.0, 4.0}};
-    ParametersGrad grads{vars, Matrix{{1.0, 6.0, 7.0, 2.0}}};
-    rmsprop->InitParameters({Sigmoid()});
+    Vector bias{{0}};
+    UpdatePack grads{vars, Matrix{{1.0, 6.0, 7.0, 2.0}}, bias, Vector{{0}}};
+    rmsprop->InitParameters({Linear(4, 1)});
     rmsprop->Update({grads}, 0);
     CheckCloseMatrix(vars, Matrix{{-0.5811, 0.4189, 1.4189, 2.4189}}, 1e-4);
 }
