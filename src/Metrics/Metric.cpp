665384fc480f6c579a492122ae3a3ed3e34805de
Васенев Владимир Олегович <vovasenev@edu.hse.ru>
2024-05-14 11:12:47 +0300

    metric tests

--- src/Metrics/Metric.cpp
+++ src/Metrics/Metric.cpp
@@ -2,21 +2,22 @@
 
 namespace neural_net {
 
-Metric::Metric(std::function<double(const Matrix &, const Matrix &)> func, const std::string &name)
+Metric::Metric(std::function<float(const Matrix &, const Matrix &)> func, const std::string &name)
     : compute_(func), name_(name) {
 }
 
-double neural_net::Metric::operator()(const Matrix &pred, const Matrix &expected) const {
+float neural_net::Metric::operator()(const Matrix &pred, const Matrix &expected) const {
     return compute_(pred, expected);
 }
 
-Metric Metric::BinaryAccuracy(double threshold) {
+Metric Metric::BinaryAccuracy(float threshold) {
     assert(threshold <= 1 && threshold >= 0);
     auto func = [threshold](const Matrix &pred, const Matrix &expected) {
         assert(pred.rows() == expected.rows() && pred.cols() == 1 && expected.cols() == 1);
-        Matrix rounded = pred.unaryExpr([threshold](float d) { return d >= threshold ? 1.f : 0.f; });
+        Matrix rounded =
+            pred.unaryExpr([threshold](float d) { return d >= threshold ? 1.f : 0.f; });
         Index right_answers = ((expected - rounded).array() == 0).count();
-        return static_cast<double>(right_answers) / pred.rows();
+        return static_cast<float>(right_answers) / pred.rows();
     };
 
     return Metric(func, "bin_acc");
@@ -39,7 +40,7 @@ Metric Metric::CategoricalAccuracy() {
             rounded.row(i) = argmax(pred.row(i));
         }
         Index right_answers = ((expected - rounded).rowwise().norm().array() == 0.).count();
-        return static_cast<double>(right_answers) / pred.rows();
+        return static_cast<float>(right_answers) / pred.rows();
     };
 
     return Metric(func, "cat_acc");
@@ -49,38 +50,57 @@ std::string Metric::GetName() const {
     return name_;
 }
 
-Metric Metric::BinaryPrecision(double threshold) {
+Metric Metric::BinaryPrecision(float threshold) {
     assert(threshold <= 1 && threshold >= 0);
     auto func = [threshold](const Matrix &pred, const Matrix &expected) {
         assert(pred.rows() == expected.rows() && pred.cols() == 1 && expected.cols() == 1);
-        Matrix rounded = pred.unaryExpr([threshold](float d) { return d >= threshold ? 1.f : 0.f; });
+        Matrix rounded =
+            pred.unaryExpr([threshold](float d) { return d >= threshold ? 1.f : 0.f; });
         Index tp = ((expected + rounded).array() == 2).count();
-        return static_cast<double>(tp) / (pred.array() == 1).count();
+        Index t = (rounded.array() == 1).count();
+        if (t != 0) {
+            return static_cast<float>(tp) / t;
+        } else {
+            return 0.f;
+        }
     };
     return Metric(func, "bin_precision");
 }
 
-Metric Metric::BinaryRecall(double threshold) {
+Metric Metric::BinaryRecall(float threshold) {
     assert(threshold <= 1 && threshold >= 0);
     auto func = [threshold](const Matrix &pred, const Matrix &expected) {
         assert(pred.rows() == expected.rows() && pred.cols() == 1 && expected.cols() == 1);
-        Matrix rounded = pred.unaryExpr([threshold](float d) { return d >= threshold ? 1.f : 0.f; });
+        Matrix rounded =
+            pred.unaryExpr([threshold](float d) { return d >= threshold ? 1.f : 0.f; });
         Index tp = ((expected + rounded).array() == 2).count();
-        return static_cast<double>(tp) / (expected.array() == 1).count();
+        Index t = (expected.array() == 1).count();
+        if (t != 0) {
+            return static_cast<float>(tp) / t;
+        } else {
+            return 0.f;
+        }
     };
     return Metric(func, "bin_recall");
 }
 
-Metric Metric::BinaryF1Score(double threshold) {
+Metric Metric::BinaryF1Score(float threshold) {
     assert(threshold <= 1 && threshold >= 0);
     auto func = [threshold](const Matrix &pred, const Matrix &expected) {
         assert(pred.rows() == expected.rows() && pred.cols() == 1 && expected.cols() == 1);
-        Matrix rounded = pred.unaryExpr([threshold](float d) { return d >= threshold ? 1.f : 0.f; });
+        Matrix rounded =
+            pred.unaryExpr([threshold](float d) { return d >= threshold ? 1.f : 0.f; });
         Index tp = ((expected + rounded).array() == 2).count();
-        double precision = static_cast<double>(tp) / (pred.array() == 1).count();
-        double recall = static_cast<double>(tp) / (expected.array() == 1).count();
-        return 2 * (precision + recall) / (precision * recall);
+        float precision = static_cast<float>(tp) / (rounded.array() == 1).count();
+        float recall = static_cast<float>(tp) / (expected.array() == 1).count();
+        float t = precision + recall;
+        if (t == 0) {
+            return 0.f;
+        } else {
+            return 2 * (precision * recall) / t;
+        }
     };
+
     return Metric(func, "bin_f1-score");
 }
 
--- src/Metrics/Metric.h
+++ src/Metrics/Metric.h
@@ -6,21 +6,21 @@ namespace neural_net {
 
 class Metric {
 public:
-    static Metric BinaryAccuracy(double threshold = 0.5);
+    static Metric BinaryAccuracy(float threshold = 0.5);
     static Metric CategoricalAccuracy();
 
-    static Metric BinaryPrecision(double threshold = 0.5);
-    static Metric BinaryRecall(double threshold = 0.5);
-    static Metric BinaryF1Score(double threshold = 0.5);
+    static Metric BinaryPrecision(float threshold = 0.5);
+    static Metric BinaryRecall(float threshold = 0.5);
+    static Metric BinaryF1Score(float threshold = 0.5);
 
-    double operator()(const Matrix& pred, const Matrix& expected) const;
+    float operator()(const Matrix& pred, const Matrix& expected) const;
 
     std::string GetName() const;
 
 private:
-    Metric(std::function<double(const Matrix&, const Matrix&)> func, const std::string& name = "");
+    Metric(std::function<float(const Matrix&, const Matrix&)> func, const std::string& name = "");
 
-    std::function<double(const Matrix&, const Matrix&)> compute_;
+    std::function<float(const Matrix&, const Matrix&)> compute_;
     std::string name_;
 };
 
